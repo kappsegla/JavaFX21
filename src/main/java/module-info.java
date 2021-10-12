b55e8c12035eb7cab755a97ebb290e2e6c43bad5
Martin <martin.blomberg@outlook.com>
2021-10-12 09:24:33 +0200

    Updates fxml file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module se.iths.java21.javafx {
     requires javafx.controls;
     requires javafx.fxml;
-
+    //requires java.base;
 
     opens se.iths.java21.javafx to javafx.fxml;
     exports se.iths.java21.javafx;
