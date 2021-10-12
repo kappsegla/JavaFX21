c2c87ec345dfc4d7e28d66899b470f3dee7dc74c
Martin <martin.blomberg@outlook.com>
2021-10-12 10:04:12 +0200

    Updates fxml file

--- src/main/java/se/iths/java21/javafx/HelloController.java
+++ src/main/java/se/iths/java21/javafx/HelloController.java
@@ -1,7 +1,10 @@
 package se.iths.java21.javafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
 
@@ -9,8 +12,21 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    private CheckBox checkBox1;
 
+    @FXML
+    protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onCheckBoxChecked()
+    {
+            welcomeText.setText("Checkbox: " +  checkBox1.isSelected());
+    }
+
+    public void mouseClicked(MouseEvent mouseEvent) {
+        System.out.println(mouseEvent.getX() + ":" + mouseEvent.getY());
+        System.out.println(mouseEvent.getSceneX() + ":" + mouseEvent.getSceneY());
+        System.out.println(mouseEvent.getScreenX() + ":" + mouseEvent.getScreenY());
     }
 }
