f6fb60e5be066d86ef56eb50ab1ff56bd775c849
Martin <martin.blomberg@outlook.com>
2021-10-12 11:49:27 +0200

    Updates fxml file with canvas

--- src/main/java/se/iths/java21/javafx/HelloController.java
+++ src/main/java/se/iths/java21/javafx/HelloController.java
@@ -5,6 +5,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -18,6 +19,7 @@ import java.util.function.UnaryOperator;
 public class HelloController {
 
     public TextField textField1;
+    public Canvas canvas;
     @FXML
     private Label welcomeText;
 
@@ -60,5 +62,11 @@ public class HelloController {
 //        String text = textField1.getText();
 //        if (text.length() > 5)
 //            textField1.setText(text.substring(0, text.length() - 1));
+    }
+
+    public void canvasClicked(MouseEvent event) {
+        System.out.println("Clicked on canvas");
+        var context = canvas.getGraphicsContext2D();
+        context.fillOval(event.getX(),event.getY(),25,25);
     }
 }
