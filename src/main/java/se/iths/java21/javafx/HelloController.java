8cd9b33c07136b05e3e7df8c08ee10dc2c32818f
Martin <martin.blomberg@outlook.com>
2021-10-12 10:49:03 +0200

    Updates fxml file

--- src/main/java/se/iths/java21/javafx/HelloController.java
+++ src/main/java/se/iths/java21/javafx/HelloController.java
@@ -4,10 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 public class HelloController {
 
+    public TextField textField1;
     @FXML
     private Label welcomeText;
 
@@ -19,14 +22,20 @@ public class HelloController {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 
-    public void onCheckBoxChecked()
-    {
-            welcomeText.setText("Checkbox: " +  checkBox1.isSelected());
+    public void onCheckBoxChecked() {
+        welcomeText.setText("Checkbox: " + checkBox1.isSelected());
     }
 
     public void mouseClicked(MouseEvent mouseEvent) {
         System.out.println(mouseEvent.getX() + ":" + mouseEvent.getY());
         System.out.println(mouseEvent.getSceneX() + ":" + mouseEvent.getSceneY());
         System.out.println(mouseEvent.getScreenX() + ":" + mouseEvent.getScreenY());
+    }
+
+    public void keyTyped(KeyEvent keyEvent) {
+        System.out.println(keyEvent.getCharacter());
+        String text = textField1.getText();
+        if (text.length() > 5)
+            textField1.setText(text.substring(0, text.length() - 1));
     }
 }
