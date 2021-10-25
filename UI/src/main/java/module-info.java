f646856281a702330447233ccf0a2334ddb8e287
Martin <martin.blomberg@outlook.com>
2021-10-25 21:37:30 +0200

    Fixes problems with running the application

---
+++ UI/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module se.iths.java21.javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.swing;
+    requires se.iths.shapes;
+
+    opens se.iths.java21.javafx to javafx.fxml;
+    exports se.iths.java21.javafx;
+}
